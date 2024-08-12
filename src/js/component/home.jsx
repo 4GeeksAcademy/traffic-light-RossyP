da919aa79e51c38aa55023e18d95b40d595de097
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-08-12 22:08:25 +0000

    Se crea la funcionalidad del semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	
+	let [color, setColor] = useState("white")
+	let [divActive, setDivActive] = useState()
+
+	function randomNum(){
+		return Math.floor(Math.random()*3)+1;
+	}
+
+	let numRandom = randomNum();
+	console.log(numRandom)
+
+	function applyLight(num){
+		if(num){
+			return  `0px 0px 50px ${color}`
+		}
+	}
+
+	function clickMe(){
+		setDivActive(randomNum())
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex flex-column justify-content-center align-items-center">
+			<div className="secondary-rectangle bg-dark" style={{width:"30px", height:"100px"}}></div>
+			<div className="primary-rectangle bg-dark rounded-3 d-flex flex-column gap-2 justify-content-center align-items-center" style={{width:"200px", height:"450px"}}>
+				<div className="bg-danger rounded-circle" style={{width:"130px", height:"130px", boxShadow: applyLight(numRandom === 1)}}></div>
+				<div className="bg-warning rounded-circle" style={{width:"130px", height:"130px", boxShadow: applyLight(numRandom === 2)}}></div>
+				<div className="bg-success rounded-circle" style={{width:"130px", height:"130px", boxShadow: applyLight(numRandom === 3)}}></div>
+			</div>
+			<div>
+				<button className="mt-3 bg-secondary text-white rounded-3" onClick={clickMe}>Click Me</button>
+			</div>
 		</div>
 	);
 };
